8d584da79c4fb6c8e30d840aa0184bf7b4b15744
lgouv <lgouv@LAPTOP-N1MDHRDO>
2018-04-24 13:59:40 +0200

    rfzesdgf

--- src/jpu2016/dogfight/view/EventPerformer.java
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -2,12 +2,12 @@ package jpu2016.dogfight.view;
 
 public class EventPerformer {
 	public EventPerformer(IOrderPerformer orderPerformer) {
-		
+		IOrderPerformer orderPerformer = new IOrderPerformer();
 	}
 	public void eventPerform(KeyEvent keyCode) {
 		
 	}
 	private UserOrder keyCodeToUserOrder(int keyCode) {
-		
+		return new UserOrder();
 	}
 }
