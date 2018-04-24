2517a841b5ece2dcc96d54b390204e62f830ccc5
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-04-24 11:27:19 +0200

    une partie de phase 3

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,26 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController {
+	private int TIME_SLEEP = 30;
+	
+	public DogfightController(IDogfightModel dogfightModel) {
+		
+	}
+	
+	public void orderPerform(UserOrder userOrder) {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	private void launchMissile( int player) {
+		
+	}
+	
+	private void gameLoop() {
+		
+	}
+	
+}
